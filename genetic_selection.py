6a3b5935484f4ce565c36722ce74ff06aaa634bd
tzachpach <tlarboni@gmail.com>
2025-01-23 16:19:01 -0500

    fixes

--- genetic_selection.py
+++ genetic_selection.py
@@ -7,8 +7,7 @@ import numpy as np
 import pandas as pd
 from deap import base, creator, tools, algorithms
 
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+from fuzzywuzzy import fuzz, process
 
 
 # Setup logging
